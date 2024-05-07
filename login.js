9b73ccb24dd26a2a2a48530954854dcec3d6cea1
davidtheblane <davidtheblane@gmail.com>
2024-05-07 08:56:13 -0300

    config login route

--- login.js
+++ login.js
@@ -2,3 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const AccountModel = mongoose.model('Account');
+
+module.exports = () => {
+  router.post('/signup', async (req, res) => {
+    const {email, password, repeatPassword} = req.body;
+    if(password === repeatPassword){
+      const user = await AccountModel.create({email, password})
+      return res.json(user);
+    }
+    res.status(400).send({ error: 'password must be equal'})
+  });
+}

49c1cd98084a95a453f8276b009fcde9ba4b64e7
davidtheblane <davidtheblane@gmail.com>
2024-05-09 19:44:17 -0300

    ensure loginRouter returns 500 if emailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -29,6 +29,17 @@ const makeEmailValidator = () => {
   return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+     isValid(){
+      throw new Error()
+    }
+  };
+
+  return new EmailValidatorSpy();
+}
+
+
 const makeAuthUseCase = () => {
   class AuthUseCaseSpy {
    async auth (email, password) {
@@ -49,6 +60,8 @@ const makeAuthUseCaseWithError = () => {
 
   return new AuthUseCaseSpy();
 }
+
+
 
 describe("LoginRouter", () => {
   test("Should return 400 if no email is provided", async () => {
@@ -221,6 +234,23 @@ describe("LoginRouter", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
 
+
+});
+
+test("Should return 500 if emailValidator throws", async () => {
+    
+  const authUseCaseSpy = makeAuthUseCase();
+  const emailValidatorSpy = makeEmailValidatorWithError();
+  const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
+  const httpRequest = {
+    body: {
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    }
+  }
+  const httpResponse = await sut.route(httpRequest);
+  expect(httpResponse.statusCode).toBe(500);
+  expect(httpResponse.body).toEqual(new ServerError());
 
 });
 

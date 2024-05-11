41e9cc480c7ed56832a924f67e7d742902087b10
davidtheblane <davidtheblane@gmail.com>
2024-05-11 10:32:10 -0300

    unsure authUsecase throws if no emaill is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,18 @@
+class AuthUseCase {
+  async auth(email) {
+    if(!email){
+      throw new Error()
+    }
+  }
+}
+
+
+describe('Auth Usecase', () => {
+  
+  test('Should throw if no email is provided', async() => {
+    const sut = new AuthUseCase()
+    await expect(async () => {await sut.auth()}).rejects.toThrow()
+
+  }) 
+  
+})

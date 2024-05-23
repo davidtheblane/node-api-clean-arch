f3f1c61b9cfd42e4ed2497af358ce064358d3bb8
davidtheblane <davidtheblane@gmail.com>
2024-05-23 08:21:55 -0300

    git test

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,15 @@
+class TokenGenerator {
+  async generate(id){
+    return null
+  }
+}
+
+describe('Token Generator', () => {
+
+  test('Should return null if jwt returns null ', async() => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+
+} )

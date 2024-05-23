28ce7ad27ef74be0a5a4a1f4af5e7071b2f00182
davidtheblane <davidtheblane@gmail.com>
2024-05-23 08:41:52 -0300

    should call JWT with correct values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,7 +1,11 @@
 module.exports = {
-  token: '',
+  token: 'any_token',
+  secret: '',
+  id: '',
   
   sign (id, secret) {
+    this.id = id
+    this.secret = secret
     return this.token
   }
 }

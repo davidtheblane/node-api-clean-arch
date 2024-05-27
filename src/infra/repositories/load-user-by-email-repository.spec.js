9992dee5fb11cd54d888c1ce5e2636833b02a3ea
davidtheblane <davidtheblane@gmail.com>
2024-05-27 08:27:10 -0300

    move loadUserByEmailRepository to its own file

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,18 +1,8 @@
 const { MongoClient } = require('mongodb')
+const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 
 let client;
 let db;
-
-class LoadUserByEmailRepository{
-  constructor (userModel){
-    this.userModel = userModel
-  }
-
-  async load(email){
-    const user = await this.userModel.findOne({email})
-    return user
-  }
-}
 
 describe("LoadUserByEmail Repository", () => {
 

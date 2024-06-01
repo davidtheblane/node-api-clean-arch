70dfefd8ca6e5018446166b2aab6df89042b1a82
davidtheblane <davidtheblane@gmail.com>
2024-06-01 18:28:11 -0300

    remove dbName from mongoHelper

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -1,11 +1,10 @@
 const { MongoClient } = require('mongodb')
 
 module.exports = {
-  async connect (uri, dbName) {
+  async connect (uri) {
     this.uri = uri
-    this.dbName = dbName
     this.client = await MongoClient.connect(uri)  
-    this.db = this.client.db(dbName)
+    this.db = this.client.db()
 
     return this.db
   },
@@ -18,7 +17,7 @@ module.exports = {
 
   async getCollection(name){
     if(!this.client) {
-      await this.connect(this.uri, this.dbName)
+      await this.connect(this.uri)
     }
     return this.db.collection(name)
   }
